f844b994c809348c02c6b1d3764261b2f59ff124
Michael Hahsler <michael@hahsler.net>
2023-02-27 12:42:59 -0600

    added DSList test file.

---
+++ Spring_2023/DSList/test_list.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "DSList.h"
+
+// write a simple singly-linked list that works with this test code.
+
+int main()
+{
+    std::cout << std::boolalpha;
+
+    DSList<int> l;
+
+    for (size_t i = 10; i > 0; --i)
+        l.push_front(i);
+
+    l.print(); // print the container's contents
+    std::cout << "empty: " << l.empty() << "\n";
+    std::cout << "size: " << l.size() << "\n\n";
+
+    std::cout << "first element: " << l.front() << "\n\n";
+
+    l.pop_front();
+    l.remove(5); // remove all elements with value 5
+    std::cout << "size (after pos and remove): " << l.size() << "\n\n";
+
+    l.print();
+
+    return 0;
+}
