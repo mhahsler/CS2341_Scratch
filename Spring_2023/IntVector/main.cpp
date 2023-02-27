9cad7ec2d9973076fec350f9b45622496654ad91
Michael Hahsler <michael@hahsler.net>
2023-02-27 12:56:37 -0600

    added missing files.

---
+++ Spring_2023/IntVector/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "IntVector.h"
+#include <cstddef>
+
+// Implement IntVector to store a sequence of integers
+// * the sequence length is fixed at construction time
+// * provide read and write operations (using the first argument as the 0-based index). 
+
+int main()
+{
+    // Create a IntVector (with length)
+    IntVector test = IntVector(5);
+
+    // read from it
+    for (size_t i = 0; i < 5; ++i)
+        std::cout << test.read(i) << " ";
+    
+    std::cout << "\n";
+
+    // write 99 in position 3 (index 2)
+    test.write(2, 99);
+
+    // this should not work!!!
+    test.write(10000000, 99);
+
+    // print the whole sequence
+    for (size_t i = 0; i < 10; ++i)
+        std::cout << test.read(i) << " ";
+    
+    std::cout << "\n";
+
+    return 0;
+}
